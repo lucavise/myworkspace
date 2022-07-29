5b9c39099e2e63cf2b2c84769a4c4aeb93ba46bc
lucavise <luca.visentin@siav.it>
2022-07-29 14:48:32 +0200

    lv - primo commit my workspace

---
+++ modules/primamvc/src/main/java/PrimaMvc/constants/PrimaMvcPortletKeys.java
@@ -0,0 +1,11 @@
+package PrimaMvc.constants;
+
+/**
+ * @author lvisentin@siav.net
+ */
+public class PrimaMvcPortletKeys {
+
+	public static final String PRIMAMVC =
+		"PrimaMvc_PrimaMvcPortlet";
+
+}
---
+++ modules/proxy-backend/src/main/java/it/infocamere/intranet/proxybackend/DettaglioServizio.java
@@ -0,0 +1,41 @@
+package it.infocamere.intranet.proxybackend;
+
+public class DettaglioServizio {
+
+    /**
+     * GET o POST
+     */
+    private String verbo;
+    /**
+     * Body del servizio da richiamare
+     */
+    private String body;
+    /**
+     * Endpoint del servizio da richiamare
+     */
+    private String uri;
+
+    public String getVerbo() {
+        return verbo;
+    }
+
+    public void setVerbo(String verbo) {
+        this.verbo = verbo;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
