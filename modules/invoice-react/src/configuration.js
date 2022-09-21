8ba712d20088b8a8a6d57f1d399814c83c132bca
lucavise <luca.visentin@siav.it>
2022-09-21 18:20:45 +0200

    lv- add configuration files

---
+++ modules/invoice-react/src/configuration.js
@@ -0,0 +1,25 @@
+const LIFERAY_PARAMS =
+  typeof _LIFERAY_PARAMS_ !== "undefined" ? _LIFERAY_PARAMS_ : {};
+
+const INTRANET_BASEURL_SVIL = "http://intranetsv.intra.infocamere.it";
+const INTRANET_BASEURL_TEST = "http://intranetts.intra.infocamere.it";
+const INTRANET_BASEURL_PROD = "https://intranet.infocamere.it";
+
+const PORTLET_INSTANCE_CONFIGURATION_SVIL = {
+  baseUrl: process.env.REACT_APP_CONFIGURATION_baseUrlSvil
+};
+
+const PORTLET_INSTANCE_CONFIGURATION_TEST = {
+  baseUrl: process.env.REACT_APP_CONFIGURATION_baseUrlTest
+};
+
+const PORTLET_INSTANCE_CONFIGURATION_PROD = {
+  baseUrl: process.env.REACT_APP_CONFIGURATION_baseUrlProd
+};
+
+export default function getConfiguration() {
+  return Object.assign(
+    PORTLET_INSTANCE_CONFIGURATION_SVIL,
+    LIFERAY_PARAMS?.configuration?.portletInstance ?? {},
+  );
+}
