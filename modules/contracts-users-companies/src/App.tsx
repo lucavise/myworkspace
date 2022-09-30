9d947d2b0b4aba07a8e55a01b7a628b8361b8b06
lucavise <luca.visentin@siav.it>
2022-09-30 18:01:14 +0200

    lv- creazione portlet typescript contratti utenti aziende

---
+++ modules/contracts-users-companies/src/App.tsx
@@ -0,0 +1,14 @@
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      contratti - aziende - utenti
+    </div>
+  );
+}
+
+export default App;
