e15d16798d33f07121beb3755284c9af9c86a858
lucavise <luca.visentin@siav.it>
2022-10-11 14:56:26 +0200

    lv- cancellate portlet di prova inutili

--- modules-react/invoice-contract-users-companies/src/App.tsx
+++ modules-react/invoice-contract-users-companies/src/App.tsx
@@ -1,13 +1,21 @@
-import { DataGrid } from 'devextreme-react';
 import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.css';
 import React from 'react';
 import './App.css';
+import { Companies } from './components/Companies';
+import { Contracts } from './components/Contracts';
+import { Users } from './components/Users';
 
 function App() {
   return (
-    <div className="App">
-      <DataGrid></DataGrid>
+    <div className="App contracts-companies-users">
+      <div className="ccu-sx">
+        <Contracts></Contracts>
+      </div>
+      <div className="ccu-dx">
+        <Companies></Companies>
+        <Users></Users>
+      </div>
     </div>
   );
 }
