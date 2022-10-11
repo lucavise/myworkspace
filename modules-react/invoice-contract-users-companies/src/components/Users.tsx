c75b216587a83f544a5137104e5145ca8156c8b9
lucavise <luca.visentin@siav.it>
2022-10-11 17:54:03 +0200

    lv - TypeScript esempio gestione degli stati con i tipi in react

--- modules-react/invoice-contract-users-companies/src/components/Users.tsx
+++ modules-react/invoice-contract-users-companies/src/components/Users.tsx
@@ -1,8 +1,14 @@
 import { DataGrid } from "devextreme-react";
+import React from "react";
 
 export const Users: React.FC = () => {
+  const [name, setName] = React.useState("");
+  const [user, setUser] = React.useState<
+    { name: string; age: number } | undefined
+  >();
   return (
     <div>
+      <div>{user && user.name}</div>
       <DataGrid></DataGrid>
     </div>
   );
